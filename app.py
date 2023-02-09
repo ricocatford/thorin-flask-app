c9dc3ef376f0609e1f08d172fa748bdfa5848e01
ricocatford <s7funoob@gmail.com>
2023-02-09 16:53:56 +0000

    Added contact page with working form.

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -18,9 +21,22 @@ def about():
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    for obj in data:
+        if obj["url"] == member_name:
+            member = obj
+    return render_template("member.html", member=member)
 
-@app.route("/contact")
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form["email"])
+        flash("Thanks {}, we've received your message.".format(request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
