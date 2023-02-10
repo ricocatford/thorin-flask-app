52f1b3213e9f4293f407efa70d5373b79385d62a
ricocatford <s7funoob@gmail.com>
2023-02-10 19:24:25 +0000

    Deployment to Heroku

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form["email"])
+        # print(request.form["email"])
         flash("Thanks {}, we've received your message.".format(request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
